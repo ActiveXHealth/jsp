33aae5e6499078ec1f2dfe16e695088d00444999
ActiveXHealth <165322665+ActiveXHealth@users.noreply.github.com>
2024-05-29 21:06:39 -0600

    Delete build/web directory

--- build/web/assets/js/rutinas.js
+++
@@ -1,7 +0,0 @@
-      function crearColorAleatorio() {
-        var red = Math.floor(Math.random() * 256);
-        var green = Math.floor(Math.random() * 256);
-        var blue = Math.floor(Math.random() * 256);
-        var colorAleatorio = "rgb(" + red + "," + green + "," + blue + ")";
-        return colorAleatorio;
-      }
